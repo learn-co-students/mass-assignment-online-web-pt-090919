ab3406b7ae5ee19dc8bae1054db72da86a5cd58b
Ally Morson <ally.morson@gmail.com>
2019-11-05 16:11:22 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
+ attributes = {name: "Bob", hair_color: "Brown" , eye_color: "Brown"}
+
 class Person
-  #your code here
+  
+  attr_accessor :name , :hair_color, :eye_color
+  
+  def initialize(attributes)
+    attributes.each {|key, value} self.send(("#{key}="), value)}
+  end
 end
